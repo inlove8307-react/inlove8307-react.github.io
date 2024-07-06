998fd848b081b46629552d76500c64e062871623
Kim Taehoon <inlove8307@gmail.com>
2024-07-06 20:40:50 +0900

    [init] next.js

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: ['styles'],
+  },
+  // assetPrefix: ".",
+  output: "export",
+};
+
+export default nextConfig;
