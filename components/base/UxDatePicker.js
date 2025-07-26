93eed3562917bb9622eeaae2d8764a2b20c41242
Kim Taehoon <inlove8307@gmail.com>
2025-07-26 19:10:43 +0900

    update

---
+++ components/base/UxDatePicker.js
@@ -0,0 +1,15 @@
+import React, { useState, useRef, useEffect } from 'react';
+import classnames from 'classnames';
+
+const UxDatePicker = ({ ref, ...props }) => {
+	const baseClassName = 'ux-datepicker';
+	const caseClassName = classnames(baseClassName, props.className);
+
+	return (
+		<div className={caseClassName}>
+			{props.children}
+		</div>
+	)
+};
+
+export default UxDatePicker;
