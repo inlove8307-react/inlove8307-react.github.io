48a1fa978ad0796c6898725bba8ca410b8137895
Kim Taehoon <inlove8307@gmail.com>
2025-08-23 06:58:07 +0900

    update

--- components/base/UxPicker.js
+++ components/base/UxPicker.js
@@ -2,7 +2,139 @@
 
 import React, { useEffect, useState, useRef } from 'react';
 import { InView } from 'react-intersection-observer';
+import { getDate, endOfMonth } from 'date-fns';
 import classnames from 'classnames';
+/* COMPONENT */
+import UxGroup from "@/components/base/UxGroup";
+
+/**
+ * <RoleDate>
+ * [props]
+ *
+ * [event]
+ *
+ */
+
+const RoleDate = ({ ref, ...props }) => {
+	const [year, setYear] = useState(0);
+	const [month, setMonth] = useState(0);
+	const [date, setDate] = useState(0);
+	const [last, setLast] = useState(0);
+
+	const handleYear = (value) => {
+		setYear(value);
+		setLast(getDate(endOfMonth(new Date(value, month - 1, date))));
+	};
+
+	const handleMonth = (value) => {
+		setMonth(value);
+		setLast(getDate(endOfMonth(new Date(year, value - 1, date))));
+	};
+
+	const handleDate = (value) => {
+		setDate(value);
+		setLast(getDate(endOfMonth(new Date(year, month - 1, value))));
+	};
+
+	useEffect(() => {
+		props.onChange && props.onChange([
+			String(year).padStart(4, '0'),
+			String(month).padStart(2, '0'),
+			String(date).padStart(2, '0'),
+		].join('.'));
+	}, [year, month, date]);
+
+	return (
+		<UxGroup className="gap16 col3">
+			<Picker
+				{...props}
+				min={props.min}
+				max={props.max}
+				suffix="년"
+				onChange={handleYear}
+			/>
+			<Picker
+				{...props}
+				min={1}
+				max={12}
+				pad={2}
+				suffix="월"
+				onChange={handleMonth}
+			/>
+			<Picker
+				{...props}
+				min={1}
+				max={last}
+				pad={2}
+				suffix="일"
+				onChange={handleDate}
+			/>
+		</UxGroup>
+	);
+};
+
+/**
+ * <RoleTime>
+ * [props]
+ *
+ * [event]
+ *
+ */
+
+const RoleTime = ({ ref, ...props }) => {
+	const [hour, setHour] = useState(0);
+	const [minute, setMinute] = useState(0);
+	const [second, setSecond] = useState(0);
+
+	const handleHour = (value) => {
+		setHour(value);
+	};
+
+	const handleMinute = (value) => {
+		setMinute(value);
+	};
+
+	const handleSecond = (value) => {
+		setSecond(value);
+	};
+
+	useEffect(() => {
+		props.onChange && props.onChange([
+			String(hour).padStart(2, '0'),
+			String(minute).padStart(2, '0'),
+			String(second).padStart(2, '0'),
+		].join(':'));
+	}, [hour, minute, second]);
+
+	return (
+		<UxGroup className="gap16 col3">
+			<Picker
+				{...props}
+				min={0}
+				max={24}
+				pad={2}
+				suffix="시"
+				onChange={handleHour}
+			/>
+			<Picker
+				{...props}
+				min={0}
+				max={59}
+				pad={2}
+				suffix="분"
+				onChange={handleMinute}
+			/>
+			<Picker
+				{...props}
+				min={0}
+				max={59}
+				pad={2}
+				suffix="초"
+				onChange={handleSecond}
+			/>
+		</UxGroup>
+	);
+};
 
 /**
  * <Picker>
@@ -15,17 +147,17 @@ import classnames from 'classnames';
 const Picker = ({ ref, ...props }) => {
 	const baseClassName = props.baseClassName;
 	const caseClassName = props.caseClassName;
-	const [min] = useState(Number(props.min));
-	const [max] = useState(Number(props.max));
+	const [min, setMin] = useState(Number(props.min));
+	const [max, setMax] = useState(Number(props.max));
 	const [data, setData] = useState([]);
 	const [value, setValue] = useState(0);
 	const listRef = useRef();
 
 	const handleClick = (event) => {
 		event.target.scrollIntoView({
-			block: "center",
-			inline: "nearest",
-			behavior: "smooth",
+			block: 'center',
+			inline: 'nearest',
+			behavior: 'smooth',
 		});
 	};
 
@@ -48,7 +180,15 @@ const Picker = ({ ref, ...props }) => {
 		while (data[data.length - 1] < max);
 
 		setData(data);
-	}, []);
+	}, [min, max]);
+
+	useEffect(() => {
+		setMin(Number(props.min));
+	}, [props.min]);
+
+	useEffect(() => {
+		setMax(Number(props.max));
+	}, [props.max]);
 
 	useEffect(() => {
 		props.onChange && props.onChange(value);
@@ -88,133 +228,6 @@ const Picker = ({ ref, ...props }) => {
 };
 
 /**
- * <Date>
- * [props]
- *
- * [event]
- *
- */
-
-const Date = ({ ref, ...props }) => {
-	const baseClassName = props.baseClassName;
-	const [year, setYear] = useState(0);
-	const [month, setMonth] = useState(0);
-	const [date, setDate] = useState(0);
-
-	const handleYear = (value) => {
-		setYear(value);
-	};
-
-	const handleMonth = (value) => {
-		setMonth(value);
-	};
-
-	const handleDate = (value) => {
-		setDate(value);
-	};
-
-	useEffect(() => {
-		props.onChange && props.onChange([
-			String(year).padStart(4, '0'),
-			String(month).padStart(2, '0'),
-			String(date).padStart(2, '0'),
-		].join('.'));
-	}, [year, month, date]);
-
-	return (
-		<div className={`${baseClassName}-date`}>
-			<Picker
-				{...props}
-				min={props.min}
-				max={props.max}
-				suffix="년"
-				onChange={handleYear}
-			/>
-			<Picker
-				{...props}
-				min={1}
-				max={12}
-				pad={2}
-				suffix="월"
-				onChange={handleMonth}
-			/>
-			<Picker
-				{...props}
-				min={1}
-				max={31}
-				pad={2}
-				suffix="일"
-				onChange={handleDate}
-			/>
-		</div>
-	);
-};
-
-/**
- * <Time>
- * [props]
- *
- * [event]
- *
- */
-
-const Time = ({ ref, ...props }) => {
-	const baseClassName = props.baseClassName;
-	const [hour, setHour] = useState(0);
-	const [minute, setMinute] = useState(0);
-	const [second, setSecond] = useState(0);
-
-	const handleHour = (value) => {
-		setHour(value);
-	};
-
-	const handleMinute = (value) => {
-		setMinute(value);
-	};
-
-	const handleSecond = (value) => {
-		setSecond(value);
-	};
-
-	useEffect(() => {
-		props.onChange && props.onChange([
-			String(hour).padStart(2, '0'),
-			String(minute).padStart(2, '0'),
-			String(second).padStart(2, '0'),
-		].join(':'));
-	}, [hour, minute, second]);
-
-	return (
-		<div className={`${baseClassName}-time`}>
-			<Picker
-				{...props}
-				min={0}
-				max={24}
-				pad={2}
-				suffix="시"
-				onChange={handleHour}
-			/>
-			<Picker
-				{...props}
-				min={0}
-				max={59}
-				pad={2}
-				suffix="분"
-				onChange={handleMinute}
-			/>
-			<Picker
-				{...props}
-				min={0}
-				max={59}
-				pad={2}
-				suffix="초"
-				onChange={handleSecond}
-			/>
-		</div>
-	);
-};
-
-/**
  * <UxPicker>
  * [props]
  *
@@ -235,9 +248,9 @@ const UxPicker = ({ ref, ...props }) => {
 
 		switch (props.role) {
 			case 'date':
-				return <Date {...props} />;
+				return <RoleDate {...props} />;
 			case 'time':
-				return <Time {...props} />;
+				return <RoleTime {...props} />;
 			default:
 				return <Picker {...props} />;
 		};
