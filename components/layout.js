64b7b97cc8dc40ec4dcb70301a400aef1e9077ba
Kim Taehoon <inlove8307@gmail.com>
2025-10-03 16:53:43 +0900

    update

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React from "react";
+import React, { useEffect } from "react";
 import classnames from 'classnames';
 import RootContextProvider from '@/context/RootContext';
 /* LAYOUT */
@@ -16,6 +16,10 @@ export default function Layout({ children }) {
 	const caseClassName = classnames({
 		guide: name === 'Guide'
 	});
+
+	useEffect(() => {
+		console.log(children);
+	}, []);
 
 	return (
 		<RootContextProvider>
