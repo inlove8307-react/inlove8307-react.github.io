3c84c0899340c929a059fe0cfb1f1f20a8e1d780
Kim Taehoon <inlove8307@gmail.com>
2026-09-18 16:25:43 +0900

    update

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,6 @@ import UxButton from "@/components/base/UxButton";
 import UxCollapse from "@/components/base/UxCollapse";
 import UxTab from "@/components/base/UxTab";
 import UxPanel from "@/components/base/UxPanel";
-/* POPUP */
-import Popup from "@/components/popup/Popup";
 
 export default function Home() {
 	const modal = useModal();
